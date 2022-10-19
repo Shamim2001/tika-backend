0a69934720526637c6fb18dba24bdca2d3a9fac4
MD. Shamim Ahmed <shamimahmedridoy2016@gmail.com>
2022-10-19 12:32:20 +0600

    tika model creations

---
+++ app/Http/Controllers/RegistratiomController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Registratiom;
+use Illuminate\Http\Request;
+
+class RegistratiomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Registratiom  $registratiom
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Registratiom $registratiom)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Registratiom  $registratiom
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Registratiom $registratiom)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Registratiom  $registratiom
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Registratiom $registratiom)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Registratiom  $registratiom
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Registratiom $registratiom)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/VaccinationCenterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vaccination_Center;
+use Illuminate\Http\Request;
+
+class VaccinationCenterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vaccination_Center  $vaccination_Center
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vaccination_Center $vaccination_Center)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vaccination_Center  $vaccination_Center
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vaccination_Center $vaccination_Center)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Vaccination_Center  $vaccination_Center
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vaccination_Center $vaccination_Center)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vaccination_Center  $vaccination_Center
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vaccination_Center $vaccination_Center)
+    {
+        //
+    }
+}
